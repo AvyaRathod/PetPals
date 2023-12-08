0c865276b64bc76b5aa7ea69007326e6530da4a0
Avya Rathod <27121364+AvyaRathod@users.noreply.github.com>
2023-12-08 20:12:55 +0530

    Initial Commit

---
+++ PetPals/PetPalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetPalsApp.swift
+//  PetPals
+//
+//  Created by Avya Rathod on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PetPalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
