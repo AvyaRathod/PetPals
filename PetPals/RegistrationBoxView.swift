64e1716820af0bbf31b03782ea04cbe5c5d1e188
SHASHWAT SINGH <ss6325@srmist.edu.in>
2023-12-24 21:33:10 +0530

    Added UserProfileView

---
+++ PetPals/RegistrationBoxView.swift
@@ -0,0 +1,47 @@
+//
+//  RegistrationBoxView.swift
+//  PetPals
+//
+//  Created by user2 on 24/12/23.
+//
+
+import SwiftUI
+
+struct RegistrationBoxView: View {
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 12)
+                .frame(height: 120)
+                .padding(.horizontal)
+            VStack{
+                HStack{
+                    VStack(alignment: .leading ,spacing: 3){
+                        Text("Register your service")
+                            .foregroundStyle(.white)
+                            .fontWeight(.semibold)
+                            .font(.title2)
+                        Text("Earn extra income and unlock new opportunities by offering your services and love to the pets.")
+                            .foregroundStyle(.white)
+                            .font(.caption)
+                    }
+                    
+                    
+                    Spacer()
+                    
+                    Button(action: {}, label: {
+                        Text("Register Now")
+                            .foregroundStyle(.white)
+                            .frame(width: 110,height: 40)
+                            .background(.appYellow)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    })
+                }
+                .padding(.horizontal,30)
+            }
+        }
+    }
+}
+
+#Preview {
+    RegistrationBoxView()
+}
